63d532094c33b623f36c8af6b500dc7a92c30ca7
Mark Vickers <markdvickers@icloud.com>
2025-03-01 10:12:52 -0700

    feat: Added basic program structure for nyc_buildings_analysis

--- nyc_buildings_analysis.py
+++ nyc_buildings_analysis.py
@@ -1,8 +1,24 @@
-import sqlalchemy as db
-import pandas as pd
+from scripts.nyc_buildings_db import *
+
+MIN_SQFT_RESIDENTIAL = 7294843
+MIN_SQFT_WAREHOUSE = 467836
+MIN_SQFT_FACTORIES = 62015
+MIN_SQFT_GARAGES = 467836
+MIN_SQFT_HOTELS = 729483
+MIN_SQFT_HOSPITALS = 467836
+MIN_SQFT_THEATERS = 62015
+MIN_SQFT_STORES = 358208
+MIN_SQFT_OFFICES = 620155
 
 def main():
-    pass
+    # Connect to Database
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    # REMOVE
+    rows = cursor.execute(q_select).fetchall()
+    [print(r) for r in rows]
+
 
 if __name__ == '__main__':
     main()
